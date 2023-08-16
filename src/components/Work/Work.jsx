464312eaa6094639bd067c93435907ca9750cb3c
LizNgoNgoc <liza.ngo@mail.ru>
2023-08-16 09:57:43 +0300

    upgrade code

--- src/components/Work/Work.jsx
+++ src/components/Work/Work.jsx
@@ -2,18 +2,13 @@ import { useState } from 'react'
 import styles from './work.module.css'
 
 export default function Work() {
-    const [colorNumb, setColorNumb] = useState("#707070")
-
-    // function handleClick() { 
-        
-    // }
-
+   
 
     return(
         <section className={styles.section}>
             <h3 className={styles.head}>КАК МЫ РАБОТАЕМ?</h3>
             <div>
-                <div className={styles.numb} onClick={()=>{setColorNumb("#F96D6B")}}>1</div>
+                <div className={styles.numb} >1</div>
                 <p className={styles.text}>Мы начинаем работать над вашим запросом сразу после первого сообщения. 
                     Вы можете связаться с нами по электронной почте, Skype или через контактную форму 
                     на этом сайте. Вы также можете воспользоваться чатом Telegram, чтобы отправить нам свои 
